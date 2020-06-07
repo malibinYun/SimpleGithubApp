dc4fcaca06d41182cab54730740ee7336fc1363e
Malibin <nightmare73@naver.com>
2020-06-08 02:24:39 +0900

    [2 주차] 핸들러를 통한 비동기 작업. 1. Thread.sleep() 위치 주의점

--- app/src/main/java/com/malibin/simplegithub/ui/sample/SampleActivity.kt
+++ app/src/main/java/com/malibin/simplegithub/ui/sample/SampleActivity.kt
@@ -2,6 +2,7 @@ package com.malibin.simplegithub.ui.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import com.malibin.simplegithub.databinding.ActivitySampleBinding
 
@@ -13,18 +14,25 @@ class SampleActivity : AppCompatActivity() {
         val binding = ActivitySampleBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val handler = Handler()
+
         val worker = Thread {
-            for (i in 1..4) {
-                Log.d("Malibin Debug", "sleep....$i")
-                binding.tvCount.text = "sleep....$i"
+            var count = 0
+            while (true) {
+                handler.post {
+                    Log.d("Malibin Debug", "sleep....$count")
+                    binding.tvCount.text = "sleep....$count"
+                    count++
+                    // Thread.sleep(1_500) 이걸 여기다가 옮기면 어떻게 될까?
+                }
                 Thread.sleep(1_500)
             }
         }
         worker.start()
         // run() 쓰면 안됩니다잉 그냥 멈춥니다. start() 는 run 실행하기 전에 호출스택에 작업 공간을 생성한 다음 run을 시작시킨다.
 
-        Log.d("Malibin Debug", "wake up!")
-        binding.tvCount.text = "wake up!"
 
+        Log.d("Malibin Debug", "wake up??")
+        binding.tvCount.text = "wake up??"
     }
 }
